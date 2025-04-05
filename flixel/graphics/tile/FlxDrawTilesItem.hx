f81535e421471fe20c5dfa7ff5bc406da12a2d89
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-04-05 19:46:31 -0300

    shit

--- flixel/graphics/tile/FlxDrawTilesItem.hx
+++ flixel/graphics/tile/FlxDrawTilesItem.hx
@@ -7,7 +7,7 @@ import flixel.graphics.tile.FlxDrawBaseItem.FlxDrawItemType;
 import flixel.math.FlxMatrix;
 import flixel.math.FlxRect;
 import flixel.system.FlxAssets.FlxShader;
-import openfl.display.Tilesheet;
+import flash.display.Tilesheet;
 import openfl.geom.ColorTransform;
 
 class FlxDrawTilesItem extends FlxDrawBaseItem<FlxDrawTilesItem>
